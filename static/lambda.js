01c39e44bddba816a8e9ca7efebb6e598bfff014
Adrian Ng <beaconwarden@outlook.com>
2017-10-23 04:07:10 +0000

    c3

--- static/lambda.js
+++ static/lambda.js
@@ -1,3 +1,13 @@
+var chart = c3.generate({
+    bindto: '#chart',
+    data: {
+      columns: [
+        ['data1', 30, 200, 100, 400, 150, 250],
+        ['data2', 50, 20, 10, 40, 15, 25]
+      ]
+    }
+});
+
 var form = document.forms.namedItem("fileinfo");
 form.addEventListener('submit', function(ev) {
     var l4 = document.getElementById("l4").value;
