117c0f1d007a458015e3e0e50a5aaf6a6ad130ad
Adrian Ng <beaconwarden@outlook.com>
2017-10-24 22:36:04 +0000

    j

---
+++ static/lam.js
@@ -0,0 +1,20 @@
+
+function loadDoc() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+      document.getElementById("demo").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("POST", "postlamb", true);
+  xhttp.send();
+  
+
+  
+    var  subm  = document.getElementById("textareaID").value
+  
+     xhttp.setRequestHeader("Content-Type", "application/json;charset=UTF-8");
+
+
+    xhttp.send(subm);
+}
