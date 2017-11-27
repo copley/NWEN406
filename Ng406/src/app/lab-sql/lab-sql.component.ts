49103ccdc5bd953d18c0f65946900a39c6b76718
Adrian Ng <beaconwarden@outlook.com>
2017-11-27 11:08:44 +0000

    added two dimentional table

--- Ng406/src/app/lab-sql/lab-sql.component.ts
+++ Ng406/src/app/lab-sql/lab-sql.component.ts
@@ -35,27 +35,34 @@ export class LabSqlComponent implements OnInit {
    * be able to query its view for the initialized paginator.
    */
   ngAfterViewInit() {
-    this.dataSource.paginator = this.paginator;
+   // this.dataSource.paginator = this.paginator;
   }
   
   public postSqlScript() :void {   
-      
-      this.restService.restPost_sql(this.Req).subscribe(
-          res => {
-            this.sqlOutput =[] ;
-            for (let i= 0 ; i < res.length ; i++ ){
-                   res[i].split('|')
-            }
-              this.sqlOutput.push(res[i]) ;debugger ;
-              
-             this.dataSource = new MatTableDataSource<sqlResponObj>(this.sqlOutput)  
-         
-          },
-          err => {
-          alert("wrong apt key or other invalid input ");
-          }
-      );
+    this.restService.restPost_sql(this.Req).subscribe(
+    res => {
+      this.sqlOutput =[] ;  let ngt =  document.getElementById("ngt");
+      for (let i= 0 ; i < res.length ; i++ )
+      {  
+        this.sqlOutput.push(res[i]) ;
+        let  row =    res[i].row.split('|') ;
+        let tr = document.createElement("TR");
+        for (let j= 0 ; j < row.length ; j++ ) {
+          let textnode = document.createTextNode(row[j]);
+          let td = document.createElement("TD"); 
+          td.setAttribute("style", "color: green; border: 4px solid #ddd ; margin :4px;");
+          td.appendChild(textnode);
+          tr.appendChild(td);
+        }
+        if (i%2==0)  tr.style.backgroundColor = "#dac0c0"; else  tr.style.backgroundColor = "#11c0c0" ;  
+        ngt.appendChild(tr);
+      }
+    },
+    err => {
+    alert("wrong apt key or other invalid input ");
     }
+    );
+  }
 
 }
 
