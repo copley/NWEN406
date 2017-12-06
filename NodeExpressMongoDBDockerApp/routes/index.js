9f4640390012533e22a6682439c996bd31fa3a66
root <root@ip-172-31-6-214.ap-southeast-2.compute.internal>
2017-12-06 01:48:32 +0000

    added nodejs

---
+++ NodeExpressMongoDBDockerApp/routes/index.js
@@ -0,0 +1,28 @@
+var express = require('express');
+var router = express.Router();
+var dockerCommandsRepository = require('../lib/dockerCommandsRepository');
+
+/* GET home page. */
+router.get('/', function(req, res, next) {
+  dockerCommandsRepository.getDockerCommands(function(err, commands) {
+      res.render('index', { dockerCommands: commands });
+  })
+  
+});
+
+
+
+router.get('/getter', function(req, res) {
+  
+  dockerCommandsRepository.getDockerCommands(function(err, commands) {
+         res.json({ dockerCommands: commands }  );     
+  })
+  
+  
+
+
+});
+
+
+
+module.exports = router;
